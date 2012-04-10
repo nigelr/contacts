afc5a791c9dac55a225564d5961685285be41150
Nigel Rausch <nigelr@brisbanerails.com>
2012-04-10 22:58:04 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contacts::Application.config.secret_token = '13fdea66a97bad1f6e32b21305d958ac6d87c8c191e1e6fdc377c7b8a7dd26a15cb3a5b7c1856a270713c209f49b3275149f19f7265cffa3b7e4be6bd9178430'
